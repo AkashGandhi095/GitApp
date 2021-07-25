2b1d8a918670ef8bb1d6c5df3af3590d0e51c070
Akash Gandhi <gandhiakash8111@gmail.com>
2021-07-25 18:46:49 +0530

    added loop logic

--- app/src/main/java/com/app/gitapp/MainActivity.kt
+++ app/src/main/java/com/app/gitapp/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MAIN_TAG" , "myTag from main branch") 
+        Log.d("MAIN_TAG" , "myTag from main branch")
+        for (i in 1..10) {
+            Log.d("MAIN_TAG2", "onCreate: $i")
+        }
     }
 }
