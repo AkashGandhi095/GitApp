403cabcbe30bc835ea8d2841fc492e0134d29c34
Akash Gandhi <gandhiakash8111@gmail.com>
2021-07-25 19:03:57 +0530

    changes from lenovo commit

--- app/src/main/java/com/app/gitapp/MainActivity.kt
+++ app/src/main/java/com/app/gitapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.app.gitapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,17 @@ class MainActivity : AppCompatActivity() {
         for (i in 1..10) {
             Log.d("MAIN_TAG2", "onCreate: $i")
         }
+        Log.d("MAIN_TAG" , "myTag from main branch")
+
+        Log.d("Lenovo_TAG" , "random : ${generateRandomList()}")
     }
+}
+
+fun generateRandomList() :ArrayList<Int> {
+    val list = arrayListOf<Int>()
+    for (i in 1..50) {
+        val rand = Random.nextInt(50000)
+        list.add(rand)
+    }
+    return list
 }
